2bf405c1d92acfa171c018c5f71c1a224b5ff12d
PBerisic <pasko.berisic@andritz.com>
2022-08-23 21:39:33 +0200

    landing page added

--- AppTemplate/src/app/app.module.ts
+++ AppTemplate/src/app/app.module.ts
@@ -6,12 +6,16 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './core/header/header.component';
 import { FooterComponent } from './core/footer/footer.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SidebarComponent } from './core/sidebar/sidebar.component';
+import { LandingComponent } from './shared/landing/landing.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    SidebarComponent,
+    LandingComponent
   ],
   imports: [
     BrowserModule,
