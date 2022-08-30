5180284bf49bee7d34912e6548cb17f1d0d14f5a
PBerisic <pasko.berisic@andritz.com>
2022-08-30 08:32:00 +0200

    primeng dependency installed

--- AppTemplate/src/app/app-routing.module.ts
+++ AppTemplate/src/app/app-routing.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ComponentDashboardComponent } from './components/component-dashboard/component-dashboard.component';
 import { LandingComponent } from './shared/landing/landing.component';
 import { LoginComponent } from './shared/login/login.component';
 import { RegisterComponent } from './shared/register/register.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
-  { path: 'dashboard', component: LandingComponent  },
+  { path: '', redirectTo: '/landing', pathMatch: 'full' },
+  { path: 'landing', component: LandingComponent  },
   { path: 'login', component: LoginComponent  },
   { path: 'register', component: RegisterComponent  },
+  { path: 'components', component: ComponentDashboardComponent  },
   
 ];
 
--- AppTemplate/src/app/app.module.ts
+++ AppTemplate/src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { PickListModule } from 'primeng/picklist';
+import { PanelModule } from 'primeng/panel'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './core/header/header.component';
@@ -9,6 +12,9 @@ import { SidebarComponent } from './core/sidebar/sidebar.component';
 import { LandingComponent } from './shared/landing/landing.component';
 import { LoginComponent } from './shared/login/login.component';
 import { RegisterComponent } from './shared/register/register.component';
+import { ComponentDashboardComponent } from './components/component-dashboard/component-dashboard.component';
+import {ButtonModule} from 'primeng/button';
+
 
 @NgModule({
   declarations: [
@@ -18,11 +24,16 @@ import { RegisterComponent } from './shared/register/register.component';
     SidebarComponent,
     LandingComponent,
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    ComponentDashboardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    PanelModule,
+    PickListModule,
+    ButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
